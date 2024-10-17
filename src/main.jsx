89c7bb489a2630f8baf50704fa4b3d6c2f9a9336
Tony Huang <170560060+tonyhchii@users.noreply.github.com>
2024-10-17 17:02:39 -0400

    react router tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Profile from "./Profile";
-import Spinach from "./Spinach.jsx";
-import Popeye from "./Popeye.jsx";
-import DefaultProfile from "./DefaultProfile.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ],
-  },
-]);
+import routes from "./routes.jsx";
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
---
+++ src/routes.jsx
@@ -0,0 +1,9 @@
+import Root from "./routes/root";
+const routes = [
+  {
+    path: "/",
+    element: <Root />,
+  },
+];
+
+export default routes;
