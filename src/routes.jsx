f53b37e83db311f19747424c0c232ea8a0b53868
Tony Huang <170560060+tonyhchii@users.noreply.github.com>
2024-10-17 17:30:05 -0400

    adding error page and contact outlet

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,18 @@
-import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Root, { loader as rootLoader } from "./routes/root";
+import Contact from "./routes/contacts";
 const routes = [
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ];
 
