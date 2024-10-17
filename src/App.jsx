edd78addba2f52dcf507c0ecde8480f692248c4b
Tony Huang <170560060+tonyhchii@users.noreply.github.com>
2024-10-17 16:26:38 -0400

    added spinach and popeye links

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  return <h1>Our first test</h1>;
-}
+import { Link } from "react-router-dom";
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile page</Link>
+            <Link to="popeye">Popeye page</Link>
+            <Link to="spinach">Spinach Page</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Profile from "./Profile";
+import Spinach from "./Spinach.jsx";
+import Popeye from "./Popeye.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+  },
+  {
+    path: "spinach",
+    element: <Spinach />,
+  },
+  {
+    path: "popeye",
+    element: <Popeye />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
